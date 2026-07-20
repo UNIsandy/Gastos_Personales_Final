97331e9bf152a56c668cc1c4a35cee35b1427422
sandy <andy67hm@gmail.com>
2026-07-19 23:50:45 -0500

    restauro footer y layout de Angie (app-body, app-footer, textura fondo)

--- ControlGastosFrontend/src/app/app.ts
+++ ControlGastosFrontend/src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, Router } from '@angular/router';
 import { HeaderComponent } from './components/header/header';
+import { FooterComponent } from './components/footer/footer';
 import { SidebarComponent } from './components/sidebar/sidebar';
 import { ToastContainerComponent } from './components/toast-container/toast-container';
 import { ConfirmDialogComponent } from './components/confirm-dialog/confirm-dialog';
@@ -8,7 +9,7 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, SidebarComponent, ToastContainerComponent, ConfirmDialogComponent, CommonModule],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, SidebarComponent, ToastContainerComponent, ConfirmDialogComponent, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
